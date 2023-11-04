2e55e1a17e55534b1badabe76e47d77e13e0ebdf
Yamawaki Takayuki <konkon.gitsune628@gmail.com>
2023-11-04 19:53:38 +0900

    add home endpoint & query filters

---
+++ src/home/dto/home.dto.ts
@@ -0,0 +1,58 @@
+import { PropertyType } from '@prisma/client';
+import { Exclude, Expose } from 'class-transformer';
+
+export class HomeResponseDto {
+  id: number;
+  address: string;
+
+  @Exclude()
+  number_of_bedrooms: number;
+
+  @Expose({ name: 'numberOfBedrooms' })
+  numberOfBedrooms() {
+    return this.number_of_bedrooms;
+  }
+
+  @Exclude()
+  number_if_bathrooms: number;
+
+  @Expose({ name: 'numberOfBathrooms' })
+  numberOfBathrooms() {
+    return this.number_if_bathrooms;
+  }
+
+  city: string;
+
+  @Exclude()
+  liseted_date: Date;
+
+  @Expose({ name: 'lisetedDate' })
+  listedDate() {
+    return this.liseted_date;
+  }
+
+  price: number;
+
+  @Exclude()
+  land_size: number;
+
+  @Expose({ name: 'landSize' })
+  landSize() {
+    return this.land_size;
+  }
+
+  property: PropertyType;
+
+  image: string;
+
+  @Exclude()
+  created_at: Date;
+  @Exclude()
+  updated_at: Date;
+  @Exclude()
+  realtor_id: number;
+
+  constructor(paritial: Partial<HomeResponseDto>) {
+    Object.assign(this, paritial);
+  }
+}
